55f2f531ed91e8f24283e0893ab88f7d9f36625d
Enric <enricvbufi@gmail.com>
2025-01-16 00:53:37 +0100

    Init commit with Blake3 Dependency

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    kotlin("jvm") version "2.0.21"
+}
+
+group = "dev.zanckor"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.github.rctcwyvrn:blake3:1.3")
+
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
