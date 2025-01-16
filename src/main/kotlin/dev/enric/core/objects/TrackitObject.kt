4088c25d37f2cf4cc0e1fc9f0288095a26e55760
Enric <enricvbufi@gmail.com>
2025-01-16 02:40:46 +0100

    Modified TrackitObject to include hash related functions, and content compression functions

---
+++ src/main/kotlin/dev/enric/core/objects/TrackitObject.kt
@@ -0,0 +1,20 @@
+package dev.enric.core.objects
+
+interface TrackitObject<T : TrackitObject<T>> {
+
+    fun encode() : Pair<Hash, ByteArray>
+
+    fun encode(writeOnDisk : Boolean = false) : Pair<Hash, ByteArray>
+
+    fun decode(hash : Hash) : T
+
+    fun generateKey() : Hash
+
+    fun compressContent() : ByteArray
+
+    fun decompressContent(compressedData : ByteArray) : String
+
+    fun printInfo() : String
+
+    fun showDifferences(newer : Hash, oldest : Hash) : String
+}
