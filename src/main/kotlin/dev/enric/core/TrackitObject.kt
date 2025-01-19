0b823c7aa063585ce5d202a24373ae70572859cc
Enric <zanckor002@gmail.com>
2025-01-20 00:09:50 +0100

    Added Tree class working with childs and paths

--- src/main/kotlin/dev/enric/core/TrackitObject.kt
+++ src/main/kotlin/dev/enric/core/TrackitObject.kt
@@ -4,19 +4,19 @@ import dev.enric.core.objects.Hash
 
 interface TrackitObject<T : TrackitObject<T>> {
 
-    fun encode() : Pair<Hash, ByteArray>
+    fun encode(): Pair<Hash, ByteArray>
 
-    fun encode(writeOnDisk : Boolean = false) : Pair<Hash, ByteArray>
+    fun encode(writeOnDisk: Boolean = false): Pair<Hash, ByteArray>
 
-    fun decode(hash : Hash) : T
+    fun decode(hash: Hash): T
 
-    fun generateKey() : Hash
+    fun generateKey(): Hash
 
-    fun compressContent() : ByteArray
+    fun compressContent(): ByteArray
 
-    fun decompressContent(compressedData : ByteArray) : String
+    fun decompressContent(compressedData: ByteArray): String
 
-    fun printInfo() : String
+    fun printInfo(): String
 
-    fun showDifferences(newer : Hash, oldest : Hash) : String
+    fun showDifferences(newer: Hash, oldest: Hash): String
 }
