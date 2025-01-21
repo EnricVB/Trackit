600428e985b099b8c45fb41f7e9a7b27658fffd1
Enric <zanckor002@gmail.com>
2025-01-22 00:41:45 +0100

    Refactored package from 'objects' to 'object'
    Changed to abstract class and added: encode; compressContent; and decompressContent, as they are always the same

--- src/main/kotlin/dev/enric/core/TrackitObject.kt
+++ src/main/kotlin/dev/enric/core/TrackitObject.kt
@@ -1,22 +1,86 @@
 package dev.enric.core
 
-import dev.enric.core.objects.Hash
+import dev.enric.Main
+import java.io.ByteArrayOutputStream
+import java.io.ObjectOutputStream
+import java.nio.file.Files
+import java.util.zip.DataFormatException
+import java.util.zip.Deflater
+import java.util.zip.Inflater
 
-interface TrackitObject<T : TrackitObject<T>> {
+abstract class TrackitObject<T : TrackitObject<T>> {
 
-    fun encode(): Pair<Hash, ByteArray>
+    open fun encode(): Pair<Hash, ByteArray> {
+        return Pair(generateKey(), compressContent())
+    }
 
-    fun encode(writeOnDisk: Boolean = false): Pair<Hash, ByteArray>
+    open fun encode(writeOnDisk: Boolean = false): Pair<Hash, ByteArray> {
+        val encodedFile = encode()
 
-    fun decode(hash: Hash): T
+        if (writeOnDisk) {
+            val objectFolder = Main.repository.getObjectsFolderPath().resolve(encodedFile.first.toString().take(2))
+            val objectFile = objectFolder.resolve(encodedFile.first.toString())
+            objectFolder.toFile().mkdir()
 
-    fun generateKey(): Hash
+            Files.write(objectFile, compressContent())
+        }
 
-    fun compressContent(): ByteArray
+        return encodedFile
+    }
 
-    fun decompressContent(compressedData: ByteArray): String
+    abstract fun decode(hash: Hash): T
 
-    fun printInfo(): String
+    abstract fun generateKey(): Hash
 
-    fun showDifferences(newer: Hash, oldest: Hash): String
+    open fun compressContent(): ByteArray {
+        val byteArrayOutputStream = ByteArrayOutputStream()
+        val objectOStream = ObjectOutputStream(byteArrayOutputStream)
+        objectOStream.writeObject(this)
+        objectOStream.flush()
+
+        val deflater = Deflater()
+        deflater.setInput(byteArrayOutputStream.toByteArray())
+        deflater.finish() // Indicates that all data has been sent
+
+        val outputStream = ByteArrayOutputStream(byteArrayOutputStream.toByteArray().size) // Stream to which is going to be sent the compressed data
+        val buffer = ByteArray(2048)
+
+        while (!deflater.finished()) {
+            val length = deflater.deflate(buffer) // Deflate does compress the data to the buffer
+            outputStream.write(buffer, 0, length) // Writes the buffered data to the outputStream
+
+            // We need a buffer since outputStream can't be treated as a ByteArray, which is needed to send the data
+            // A ByteArray is needed, and we can't send it all directly to a single ByteArray as we don't know the final length needed
+        }
+
+        return outputStream.toByteArray()
+    }
+
+    open fun decompressContent(compressedData: ByteArray): ByteArray? {
+        val inflater = Inflater()
+        inflater.setInput(compressedData)
+        // This does not need all the data to be decompressed before starting
+
+        val outputStream =
+            ByteArrayOutputStream(compressedData.size) // Stream to which is going to be sent the decompressed data
+        val buffer = ByteArray(1024)
+
+        try {
+            while (!inflater.finished()) {
+                val length = inflater.inflate(buffer) // Decompress the data into the buffer
+                outputStream.write(buffer, 0, length) // Writes the decompressed data into the outputStream
+            }
+
+            return outputStream.toByteArray()
+        } catch (e: DataFormatException) {
+            e.printStackTrace()
+            println("Error decompressing data. Check that the data is compressed correctly.")
+        }
+
+        return null
+    }
+
+    abstract fun printInfo(): String
+
+    abstract fun showDifferences(newer: Hash, oldest: Hash): String
 }
