c5500935b199d4e6284107b390d5bc459e070bd7
Enric <enricvbufi@gmail.com>
2025-03-11 13:32:42 +0100

    Feta: Added few methods on BranchIndex for Head searching.

--- src/main/kotlin/dev/enric/util/index/BranchIndex.kt
+++ src/main/kotlin/dev/enric/util/index/BranchIndex.kt
@@ -4,7 +4,10 @@ import dev.enric.core.Hash
 import dev.enric.core.Hash.HashType.BRANCH
 import dev.enric.core.Hash.HashType.USER
 import dev.enric.domain.Branch
+import dev.enric.domain.Commit
+import dev.enric.util.index.CommitIndex.repositoryFolderManager
 import dev.enric.util.repository.RepositoryFolderManager
+import java.nio.file.Files
 import kotlin.io.path.ExperimentalPathApi
 import kotlin.io.path.PathWalkOption
 import kotlin.io.path.isDirectory
@@ -42,6 +45,39 @@ object BranchIndex {
      */
     fun branchAlreadyExists(branchName: String): Boolean {
         return getAllBranches().any { Branch.newInstance(it).name == branchName }
+    }
+
+    /**
+     * Saves the Branch Head Pointer to the given commit.
+     *
+     * @param commit A [Hash] object representing the commit to set as the branch head.
+     */
+    fun setBranchHead(commit: Hash) {
+        Files.writeString(repositoryFolderManager.getBranchHeadPath(), commit.string)
+    }
+
+    /**
+     * Retrieves the Branch Head Pointer.
+     * This is the commit the branch is pointing to.
+     *
+     * @return A [Commit] object representing the branch head.
+     */
+    fun getBranchHead() : Commit? {
+        val hashString = Files.readString(repositoryFolderManager.getBranchHeadPath())
+
+        return if(hashString.isNullOrEmpty()) null else Commit.newInstance(Hash(hashString))
+    }
+
+    /**
+     * Retrieves the current branch.
+     *
+     * This is the branch the Working Directory is currently on.
+     * In case this is a new repository and no branch has been created yet, it will return 'main' branch.
+     *
+     * @return A [Branch] object representing the current branch.
+     */
+    fun getCurrentBranch() : Branch {
+        return if(getBranchHead()?.branch != null) Branch.newInstance(getBranchHead()?.branch!!) else Branch("main")
     }
 
     /**
