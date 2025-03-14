1df969bafa72232057703433097f2a4aa0b03742
EnricVB <enricvbufi@gmail.com>
2025-03-14 14:52:55 +0100

    Refactor: Added a small description to FileStatusTypes in order to show it on status command

--- src/main/kotlin/dev/enric/util/common/FileStatusTypes.kt
+++ src/main/kotlin/dev/enric/util/common/FileStatusTypes.kt
@@ -5,47 +5,63 @@ package dev.enric.util.common
  * Each status indicates the state of a file in the working directory, staging area, or repository.
  *
  * @param symbol The shorthand symbol used to represent the file status.
+ * @param description A human-readable description of the status.
  */
-enum class FileStatusTypes(val symbol: String) {
+enum class FileStatusTypes(val symbol: String, val description: String) {
+
     /**
      * The file is not being tracked by Trackit. It exists in the working directory but has not been added to the index.
-     * Represented by `?` in Trackit status.
      */
-    UNTRACKED("?"),
+    UNTRACKED("?",
+        """Untracked files:
+            |   (use "trackit stage <file>..." to include in what will be committed)""".trimMargin()),
 
     /**
      * The file is tracked and has not been modified since the last commit.
-     * Represented by a space character in Trackit status.
      */
-    UNMODIFIED(" "),
+    UNMODIFIED(" ", """Files up to date:"""),
 
     /**
      * The file has been modified but is not yet staged for commit.
-     * Represented by `M` in Trackit status.
      */
-    MODIFIED("M"),
+    MODIFIED("M",
+        """Files with changes:
+            |   (use "trackit stage <file>..." to update what will be committed)""".trimMargin()),
 
     /**
      * The file has been modified and staged for the next commit.
-     * Represented by `S` (commonly `A` or `M` in Trackit, but using `S` for "Staged" here).
      */
-    STAGED("S"),
+    STAGED("S",
+        """Changes to be committed:
+            |   (use "trackit commit" to commit changes)
+            |   (use "trackit unstage" to unstage changes)""".trimMargin()),
 
     /**
      * The file has been deleted and the deletion has been staged.
-     * Represented by `D` in Trackit status.
      */
-    DELETE("D"),
+    DELETE("D",
+        """Deleted files:
+            |   (use "trackit restore <file>..." to restore the file)""".trimMargin()),
 
     /**
      * The file has been renamed or moved.
-     * Represented by `R` in Trackit status.
      */
-    RENAMED("R"),
+    RENAMED("R",
+        """Renamed files:
+            |   (use "trackit restore <file>..." to restore the file)""".trimMargin()),
 
     /**
      * The file is ignored based on `.ignore` rules and will not be tracked.
-     * Represented by `I` (Trackit typically doesn't show ignored files unless explicitly requested).
      */
-    IGNORED("I")
+    IGNORED("I", "Ignored files");
+
+    companion object {
+        /**
+         * Retrieves a [FileStatusTypes] from its symbol.
+         * Returns null if the symbol does not match any status.
+         */
+        fun fromSymbol(symbol: String): FileStatusTypes? {
+            return entries.find { it.symbol == symbol }
+        }
+    }
 }
