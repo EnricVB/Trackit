6865b99327ea49d7bd3f0f5a6e854ffc1df048db
Enric <enricvbufi@gmail.com>
2025-03-14 14:53:40 +0100

    Feat: Added StatusHandler to show the status of all files in working area.

--- src/main/kotlin/dev/enric/core/repo/staging/StatusHandler.kt
+++ src/main/kotlin/dev/enric/core/repo/staging/StatusHandler.kt
@@ -2,50 +2,152 @@ package dev.enric.core.repo.staging
 
 import dev.enric.core.repo.ignore.IgnoreHandler
 import dev.enric.domain.Hash
-import dev.enric.domain.Hash.HashType.CONTENT
-import dev.enric.domain.objects.Commit
 import dev.enric.domain.objects.Content
 import dev.enric.domain.objects.Tree
+import dev.enric.domain.objects.User
+import dev.enric.logger.Logger
+import dev.enric.util.common.ColorUtil
 import dev.enric.util.common.FileStatusTypes
+import dev.enric.util.common.SerializablePath
+import dev.enric.util.index.BranchIndex
 import dev.enric.util.index.CommitIndex
 import dev.enric.util.repository.RepositoryFolderManager
 import java.io.File
+import kotlin.io.path.ExperimentalPathApi
+import kotlin.io.path.PathWalkOption
+import kotlin.io.path.walk
 
+/**
+ * Handles the status of files in the repository.
+ * This class provides functionality to check the state of files in the working directory,
+ * determine which files have been modified, staged, or ignored, and display this information.
+ */
 object StatusHandler {
+    val repositoryFolderManager = RepositoryFolderManager()
 
-    fun getStatus(file: File): FileStatusTypes {
-        val fileContent = file.readText()
-        val hash = Content(fileContent.toByteArray()).generateKey()
+    /**
+     * Prints the current repository status to the logger.
+     * It displays:
+     * - The current branch name.
+     * - The latest commit details (hash, author, date).
+     * - The list of files grouped by their status (modified, staged, untracked, etc.).
+     */
+    fun printStatus() {
+        val statusMap = getFilesStatus()
+        val branch = BranchIndex.getCurrentBranch()
+        val currentCommit = CommitIndex.getCurrentCommit()
 
+        Logger.log("On branch ${branch.name}")
+
+        if (currentCommit != null) {
+            Logger.log("Current commit: ${currentCommit.encode().first}")
+            Logger.log("Author: ${User.newInstance(currentCommit.author).name}")
+            Logger.log("Date: ${currentCommit.date}")
+        }
+
+        statusMap.forEach { (status, files) ->
+            val pathToShow = files.map {
+                SerializablePath.of(it.path).relativePath(repositoryFolderManager.getInitFolderPath()).toString()
+            }
+
+            Logger.log("")
+            Logger.log(status.description)
+
+            pathToShow.forEach { file -> Logger.log("\t[${status.symbol}] ${ColorUtil.message(file)}") }
+        }
+
+        if (StagingHandler.getStagedFiles().isEmpty()) {
+            Logger.log("")
+            Logger.log("no changes added to commit (use \"trackit stage\" and/or \"trackit commit -a\")")
+        }
+    }
+
+    /**
+     * Retrieves the status of all files in the working directory.
+     *
+     * @return A map where each key is a [FileStatusTypes] representing the file status,
+     *         and each value is a list of files that fall under that status.
+     */
+    @OptIn(ExperimentalPathApi::class)
+    fun getFilesStatus(): Map<FileStatusTypes, List<File>> {
+        val statusMap = mutableMapOf<FileStatusTypes, MutableList<File>>()
+
+        repositoryFolderManager.getInitFolderPath()
+            .walk(PathWalkOption.INCLUDE_DIRECTORIES)
+            .filterNot {
+                it.toFile().isDirectory ||
+                        it.toRealPath().startsWith(repositoryFolderManager.getTrackitFolderPath()) ||
+                        it.toRealPath().startsWith(repositoryFolderManager.getSecretKeyPath()) ||
+                        it.toRealPath() == repositoryFolderManager.getInitFolderPath()
+            }
+            .forEach { path ->
+                val file = path.toFile()
+                val status = getStatus(file)
+
+                statusMap.getOrPut(status) { mutableListOf() }.add(file)
+            }
+
+        return statusMap.mapValues { (_, files) -> files.sortedBy { it.path } }
+    }
+
+    /**
+     * Retrieves the status of all files in the working directory.
+     *
+     * @return A map where each key is a [FileStatusTypes] representing the file status,
+     *         and each value is a list of files that fall under that status.
+     */
+    private fun getStatus(file: File): FileStatusTypes {
+        if (!file.exists()) return FileStatusTypes.DELETE
+        if (IgnoreHandler.isIgnored(file.toPath())) return FileStatusTypes.IGNORED
+
+        val hash = Content(file.readBytes()).generateKey()
         val contentExists = contentExists(hash)
         val isUpToDate = isUpToDate(file)
         val isStaged = StagingHandler.getStagedFiles().any { it.first == hash }
-        val isIgnored = IgnoreHandler.isIgnored(file.toPath())
 
         return when {
             isStaged -> FileStatusTypes.STAGED
             contentExists && isUpToDate -> FileStatusTypes.UNMODIFIED
             contentExists && !isUpToDate -> FileStatusTypes.MODIFIED
-            isIgnored -> FileStatusTypes.IGNORED
             else -> FileStatusTypes.UNTRACKED
         }
     }
 
-    fun contentExists(hash: Hash): Boolean {
-        val contentFolder = RepositoryFolderManager().getObjectsFolderPath().resolve(CONTENT.hash.toString())
-        val contentFile = contentFolder.resolve(hash.toString())
+    /**
+     * Checks if a given content has been added to the repository.
+     *
+     * @param hash The hash of the content to check.
+     * @return `true` if the content exists in the repository, `false` otherwise.
+     */
+    private fun contentExists(hash: Hash): Boolean {
+        val currentCommit = CommitIndex.getCurrentCommit() ?: return false
 
-        return contentFile.toFile().exists()
+        currentCommit.tree.forEach { treeHash ->
+            val tree = Tree.newInstance(treeHash)
+
+            if (tree.content == hash) {
+                return true
+            }
+        }
+
+        return false
     }
 
-    fun isUpToDate(file: File): Boolean {
-        val currentCommit = CommitIndex.getCurrentCommit()?.let { Commit.newInstance(it) } ?: return true
+    /**
+     * Determines if a file is up to date with the latest commit.
+     *
+     * @param file The file to check.
+     * @return `true` if the file content matches the latest commit, `false` otherwise.
+     */
+    private fun isUpToDate(file: File): Boolean {
+        val currentCommit = CommitIndex.getCurrentCommit() ?: return true
 
         currentCommit.tree.forEach { tree ->
             val treeObject = Tree.newInstance(tree)
-            val treePath = treeObject.serializablePath.toString()
+            val treePath = treeObject.serializablePath.relativePath(repositoryFolderManager.getInitFolderPath())
+            val filePath = SerializablePath.of(file.path).relativePath(repositoryFolderManager.getInitFolderPath())
 
-            if (treePath == file.path) {
+            if (treePath == filePath) {
                 val treeContent = treeObject.content
                 val fileContent = Content(file.readText().toByteArray())
 
