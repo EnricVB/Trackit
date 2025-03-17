a7f4069617821e909b78d8865886010b846f4489
Enric <enricvbufi@gmail.com>
2025-03-17 20:20:13 +0100

    Feat: Added Docker and scripts to run it

---
+++ docker/run-trackit.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "🚀 Build de Trackit Docker..."
+docker-compose build
+
+echo "🐳 Iniciando contenedor Trackit..."
+docker-compose up -d
+
+echo "✅ Trackit corriendo. Puedes conectar por SSH a puerto 2222."
