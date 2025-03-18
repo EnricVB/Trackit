f25d5afaf86b7bc0e761260b335634c0b0d8b6b0
Enric <enricvbufi@gmail.com>
2025-03-18 14:26:18 +0100

    Refactor: Moved most status related methods to FileStatus
     Instead of having them in StatusHandler, those methods have been moved to FileStatus

--- src/main/kotlin/dev/enric/core/handler/repo/staging/StatusHandler.kt
+++ src/main/kotlin/dev/enric/core/handler/repo/staging/StatusHandler.kt
@@ -1,21 +1,16 @@
 package dev.enric.core.handler.repo.staging
 
 import dev.enric.core.handler.repo.ignore.IgnoreHandler
-import dev.enric.domain.Hash
 import dev.enric.domain.objects.Content
-import dev.enric.domain.objects.Tree
 import dev.enric.domain.objects.User
 import dev.enric.logger.Logger
 import dev.enric.util.common.ColorUtil
-import dev.enric.util.common.FileStatusTypes
+import dev.enric.util.common.FileStatus
+import dev.enric.util.common.FileStatus.*
 import dev.enric.util.common.SerializablePath
 import dev.enric.util.index.BranchIndex
 import dev.enric.util.index.CommitIndex
 import dev.enric.util.repository.RepositoryFolderManager
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.runBlocking
 import java.io.File
 import kotlin.io.path.ExperimentalPathApi
 import kotlin.io.path.PathWalkOption
@@ -69,12 +64,12 @@ object StatusHandler {
     /**
      * Retrieves the status of all files in the working directory.
      *
-     * @return A map where each key is a [FileStatusTypes] representing the file status,
+     * @return A map where each key is a [FileStatus] representing the file status,
      *         and each value is a list of files that fall under that status.
      */
     @OptIn(ExperimentalPathApi::class)
-    fun getFilesStatus(): Map<FileStatusTypes, List<File>> {
-        val statusMap = mutableMapOf<FileStatusTypes, MutableList<File>>()
+    fun getFilesStatus(): Map<FileStatus, List<File>> {
+        val statusMap = mutableMapOf<FileStatus, MutableList<File>>()
 
         repositoryFolderManager.getInitFolderPath()
             .walk(PathWalkOption.INCLUDE_DIRECTORIES)
@@ -87,133 +82,15 @@ object StatusHandler {
             }
             .forEach { path ->
                 val file = path.toFile()
-                val status = getStatus(file)
+                val status = FileStatus.getStatus(file)
 
                 statusMap.getOrPut(status) { mutableListOf() }.add(file)
             }
 
-        getDeletedFiles().forEach {
-            statusMap.getOrPut(FileStatusTypes.DELETE) { mutableListOf() }.add(it)
+        FileStatus.getDeletedFiles().forEach {
+            statusMap.getOrPut(DELETE) { mutableListOf() }.add(it)
         }
 
         return statusMap.mapValues { (_, files) -> files.sortedBy { it.path } }
-    }
-
-    /**
-     * Retrieves the status of all files in the working directory.
-     *
-     * @return A map where each key is a [FileStatusTypes] representing the file status,
-     *         and each value is a list of files that fall under that status.
-     */
-    private fun getStatus(file: File): FileStatusTypes {
-        if (!file.exists()) return FileStatusTypes.DELETE
-        if (IgnoreHandler.isIgnored(file.toPath())) return FileStatusTypes.IGNORED
-
-        val hash = Content(file.readBytes()).generateKey()
-        val contentExists = contentExists(hash)
-        val isUpToDate = isUpToDate(file)
-        val isStaged = StagingHandler.getStagedFiles().any { it.first == hash }
-
-        return when {
-            isStaged -> FileStatusTypes.STAGED
-            contentExists && isUpToDate -> FileStatusTypes.UNMODIFIED
-            contentExists && !isUpToDate -> FileStatusTypes.MODIFIED
-            else -> FileStatusTypes.UNTRACKED
-        }
-    }
-
-    /**
-     * Checks if a given content has been added to the repository.
-     *
-     * @param hash The hash of the content to check.
-     * @return `true` if the content exists in the repository, `false` otherwise.
-     */
-    private fun contentExists(hash: Hash): Boolean {
-        val currentCommit = CommitIndex.getCurrentCommit() ?: return false
-
-        currentCommit.tree.forEach { treeHash ->
-            val tree = Tree.newInstance(treeHash)
-
-            if (tree.content == hash) {
-                return true
-            }
-        }
-
-        return false
-    }
-
-    /**
-     * Retrieves the list of files that have been deleted from the repository.
-     * This is done by comparing the current commit tree with the working directory.
-     *
-     * @return A list of files that have been deleted.
-     */
-    fun getDeletedFiles(): List<File> {
-        val currentCommit = CommitIndex.getCurrentCommit() ?: return emptyList()
-        val initFolderPath = repositoryFolderManager.getInitFolderPath()
-
-        return runBlocking {
-            currentCommit.tree.map { treeHash ->
-                async (Dispatchers.IO) {
-                    val treeObject = Tree.newInstance(treeHash)
-                    val treePathRelative = treeObject.serializablePath.relativePath(initFolderPath)
-                    val filePath = initFolderPath.resolve(treePathRelative)
-                    val file = filePath.toFile()
-
-                    if (!file.exists()) file else null
-                }
-            }.awaitAll().filterNotNull()
-        }
-    }
-
-    /**
-     * Determines if a file has been deleted from the repository.
-     * This is done by comparing the current commit tree with the working directory.
-     *
-     * @param file The file to check.
-     * @return `true` if the file has been deleted, `false` otherwise.
-     * @see CommitIndex
-     */
-    fun hasBeenDeleted(file: File): Boolean {
-        val currentCommit = CommitIndex.getCurrentCommit() ?: return false
-
-        currentCommit.tree.forEach { tree ->
-            val treeObject = Tree.newInstance(tree)
-            val treePath = treeObject.serializablePath.relativePath(repositoryFolderManager.getInitFolderPath())
-            val filePath = SerializablePath.of(file.path).relativePath(repositoryFolderManager.getInitFolderPath())
-
-            if (treePath == filePath) {
-                if (!file.exists()) {
-                    return true
-                }
-            }
-        }
-
-        return false
-    }
-
-    /**
-     * Determines if a file is up to date with the latest commit.
-     *
-     * @param file The file to check.
-     * @return `true` if the file content matches the latest commit, `false` otherwise.
-     */
-    private fun isUpToDate(file: File): Boolean {
-        val currentCommit = CommitIndex.getCurrentCommit() ?: return true
-
-        currentCommit.tree.forEach { tree ->
-            val treeObject = Tree.newInstance(tree)
-            val treePath = treeObject.serializablePath.relativePath(repositoryFolderManager.getInitFolderPath())
-            val filePath = SerializablePath.of(file.path).relativePath(repositoryFolderManager.getInitFolderPath())
-
-            if (treePath == filePath) {
-                val treeContent = treeObject.content
-                val fileContent = Content(file.readText().toByteArray())
-
-                return treeContent == fileContent.generateKey()
-            }
-        }
-
-        return false
     }
 }
