36fa267a796b0abb87000f198a9118ccccc8189b
enric.velasco <enric.velasco@csa.es>
2025-03-18 13:51:02 +0100

    Feat: Added showDiff to show all differences between two texts

--- src/main/kotlin/dev/enric/util/common/Utility.kt
+++ src/main/kotlin/dev/enric/util/common/Utility.kt
@@ -1,11 +1,40 @@
 package dev.enric.util.common
 
+import com.github.difflib.text.DiffRow
+import com.github.difflib.text.DiffRow.Tag.*
+import com.github.difflib.text.DiffRowGenerator
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
+
 object Utility {
 
-    fun getLogDateFormat(format: String) : String {
+    fun getLogDateFormat(format: String): String {
         return LocalDateTime.now().format(DateTimeFormatter.ofPattern(format))
+    }
+
+    fun showDiff(text1: String, text2: String): String {
+        val generator = DiffRowGenerator.create()
+            .showInlineDiffs(false)
+            .ignoreWhiteSpaces(true)
+            .reportLinesUnchanged(true)
+            .mergeOriginalRevised(false)
+            .inlineDiffByWord(false)
+            .replaceOriginalLinefeedInChangesWithSpaces(true)
+            .build()
+
+        val rows: List<DiffRow> = generator.generateDiffRows(text1.split("\n"), text2.split("\n"))
+        val diff = StringBuilder()
+
+        rows.forEach {
+            when (it.tag ?: EQUAL) {
+                INSERT -> diff.append("+ ${ColorUtil.insertLine(it.newLine)}\n")
+                DELETE -> diff.append("- ${ColorUtil.deleteLine(it.oldLine)}\n")
+                CHANGE -> diff.append("- ${it.oldLine}\n+ ${it.newLine}\n")
+                else -> diff.append("  ${it.oldLine}\n")
+            }
+        }
+
+        return diff.toString()
     }
 }
