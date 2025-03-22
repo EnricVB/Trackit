758f98c799c1c1cfbd25c1cb25ad8371163efe8f
Enric <enricvbufi@gmail.com>
2025-03-22 23:14:50 +0100

    Feat: Added all diff options.
    Difference between:
    - Working Area vs Staging Area
    - Staging Area vs HEAD
    - Staging Area vs Commit
    - Two commits
    - Two branches

--- src/main/kotlin/dev/enric/command/administration/Diff.kt
+++ src/main/kotlin/dev/enric/command/administration/Diff.kt
@@ -3,7 +3,11 @@ package dev.enric.command.administration
 import dev.enric.command.TrackitCommand
 import dev.enric.core.handler.administration.DiffHandler
 import dev.enric.domain.Hash
+import dev.enric.domain.objects.Branch
 import dev.enric.domain.objects.Commit
+import dev.enric.exceptions.IllegalStateException
+import dev.enric.logger.Logger
+import dev.enric.util.index.BranchIndex
 import picocli.CommandLine.*
 
 /**
@@ -15,10 +19,15 @@ import picocli.CommandLine.*
  *
  * Usage examples:
  *   trackit diff                              -> Compare Working Area vs Staging Area (default)
+ *
  *   trackit diff -w                           -> Explicitly compare Working Area vs Staging Area
+ *
  *   trackit diff -s                           -> Compare Staging Area vs HEAD (last commit)
+ *
  *   trackit diff <commit>                     -> Compare Working Area vs specified commit
+ *
  *   trackit diff <commit1> <commit2>          -> Compare two commits
+ *
  *   trackit diff <branch1> <branch2>          -> Compare two branches
  *
  * Additional options allow filtering and customizing the output.
@@ -88,21 +97,50 @@ class Diff : TrackitCommand() {
 
         // Validate that commit/branch parameters are not used with --working-area or --staged
         if ((showWorkingAreaDiff || showStagedDiff) && hashes.isNotEmpty()) {
-            println("Error: Cannot use --working-area or --staged together with commit or branch parameters.")
-            return 1
+            throw IllegalStateException("Cannot use --working-area or --staged together with commit or branch parameters.")
         }
 
         val diffHandler = DiffHandler(fileFilter)
 
         // Execute the appropriate diff command based on the provided parameters
         when {
-            showWorkingAreaDiff -> diffHandler.executeDiffInWorkdir()
-            showStagedDiff -> diffHandler.executeDIffInStagingArea()
-            //hashes.size == 1 -> diffHandler.executeDiffBetweenWorkdirAndCommit()
-            hashes.size == 2 -> diffHandler.executeDiffBetweenCommits(
-                Commit.newInstance(Hash(hashes[0])),
-                Commit.newInstance(Hash(hashes[1]))
-            )
+            showWorkingAreaDiff -> {
+                Logger.log("Diff STAGING vs WORKDIR\n")
+                diffHandler.executeDiffInWorkdir()
+            }
+
+            showStagedDiff -> {
+                Logger.log("Diff HEAD vs WORKDIR\n")
+                val currentBranch = BranchIndex.getCurrentBranch()
+                val headCommit = currentBranch.let { BranchIndex.getBranchHead(it.generateKey()) }
+
+                headCommit?.let { diffHandler.executeDiffBetweenWorkdirAndCommit(it) }
+            }
+
+            hashes.size == 1 -> {
+                Logger.log("Diff ${hashes[0]} vs WORKDIR\n")
+                diffHandler.executeDiffBetweenWorkdirAndCommit(Commit.newInstance(Hash(hashes[0])))
+            }
+
+            hashes.size == 2 -> {
+                Logger.log("Diff ${hashes[0]} vs ${hashes[1]}\n")
+                val isFirstHashBranch = hashes[0].startsWith(Hash.HashType.BRANCH.hash.string)
+                val isSecondHashBranch = hashes[1].startsWith(Hash.HashType.BRANCH.hash.string)
+
+                if (isFirstHashBranch && isSecondHashBranch) {
+                    diffHandler.executeDiffBetweenBranches(
+                        Branch.newInstance(Hash(hashes[0])),
+                        Branch.newInstance(Hash(hashes[1]))
+                    )
+                } else if (isFirstHashBranch || isSecondHashBranch) {
+                    throw IllegalStateException("Cannot compare a branch with a commit.")
+                } else {
+                    diffHandler.executeDiffBetweenCommits(
+                        Commit.newInstance(Hash(hashes[0])),
+                        Commit.newInstance(Hash(hashes[1]))
+                    )
+                }
+            }
         }
 
         return 0
