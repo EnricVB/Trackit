3b3f76d6e25d036d2335617eb43022ecb83355b0
Enric <enricvbufi@gmail.com>
2025-03-24 10:53:48 +0100

    Fix: Dockerfile not working on Linux

--- docker/run-trackit.sh
+++ docker/run-trackit.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 echo "🚀 Build de Trackit Docker..."
-docker compose build
+docker compose build --no-cache
 
 echo "🐳 Iniciando contenedor Trackit..."
 docker compose up -d
-
-echo "✅ Trackit corriendo. Puedes conectar por SSH a puerto 2222."
-ssh trackit@localhost -p 2222
