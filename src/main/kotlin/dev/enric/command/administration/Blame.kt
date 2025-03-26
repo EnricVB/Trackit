4603410054f30bc5440840edc96f38ac40addd45
enric.velasco <enric.velasco@csa.es>
2025-03-26 08:45:20 +0100

    Refactor: Added documentation to Blame command

--- src/main/kotlin/dev/enric/command/administration/Blame.kt
+++ src/main/kotlin/dev/enric/command/administration/Blame.kt
@@ -3,10 +3,15 @@ package dev.enric.command.administration
 import dev.enric.command.TrackitCommand
 import dev.enric.core.handler.administration.BlameHandler
 import dev.enric.exceptions.IllegalStateException
+import dev.enric.logger.Logger
 import dev.enric.util.index.BranchIndex
 import picocli.CommandLine.*
 import java.nio.file.Path
 
+/**
+ * The Blame command identifies the author responsible for changes in a given file.
+ * It retrieves the latest commit for the current branch and determines who made the changes.
+ */
 @Command(
     name = "blame",
     description = ["Indicates who is responsible for the changes in a file."],
@@ -18,9 +23,17 @@ import java.nio.file.Path
 
 class Blame : TrackitCommand() {
 
+    /**
+     * The file path to analyze and determine authorship.
+     */
     @Parameters(index = "0", paramLabel = "File", description = ["The file path to the file to blame."])
     lateinit var filePath: Path
 
+    /**
+     * Executes the blame command, retrieving the author information for the specified file.
+     *
+     * @return Exit code 0 if successful, throws an exception if the file is not found.
+     */
     override fun call(): Int {
         super.call()
 
@@ -29,7 +42,7 @@ class Blame : TrackitCommand() {
         val file = filePath.toFile().takeIf { it.exists() }
             ?: throw IllegalStateException("No file found at $filePath")
 
-        println(BlameHandler.blame(file, commit))
+        Logger.log(BlameHandler.blame(file, commit))
 
         return 0
     }
