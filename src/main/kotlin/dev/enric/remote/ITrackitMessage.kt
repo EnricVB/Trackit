7429ac0de2fb8aa3a5db8771dea715d57e8938ec
EnricVB <enricvbufi@gmail.com>
2025-05-07 12:22:41 +0200

    Refactor: Add coroutine exception handling to ITrackitMessage and simplify encode/decode documentation

--- src/main/kotlin/dev/enric/remote/ITrackitMessage.kt
+++ src/main/kotlin/dev/enric/remote/ITrackitMessage.kt
@@ -1,6 +1,8 @@
 package dev.enric.remote
 
+import dev.enric.logger.Logger
 import dev.enric.remote.network.serialize.MessageFactory
+import kotlinx.coroutines.CoroutineExceptionHandler
 import java.net.Socket
 
 
@@ -26,31 +28,25 @@ interface ITrackitMessage<T> {
     val id: MessageFactory.MessageType
     var payload: T
 
+    val coroutineExceptionHandler: CoroutineExceptionHandler
+        get() = CoroutineExceptionHandler { _, exception ->
+            Logger.error("Caught exception in coroutine: ${exception.message}")
+        }
+
     /**
-     * Encodes the object into a pair of a Hash and a ByteArray.
-     * The Hash is the key that is going to be used to store the object in the repository.
-     *
-     * The ByteArray is the compressed object.
-     * The data is serialized using ObjectOutputStream and written to a ByteArrayOutputStream.
-     *
-     * @return ByteArray with the serialized data.
+     * Encodes the object to a ByteArray.
+     * The ByteArray is the serialized object that is going to be sent using ObjectOutputStream.
      */
     fun encode(): ByteArray
 
     /**
-     * Decodes the object from a ByteArray.
-     * The ByteArray is the serialized object that is going to be deserialized using ObjectInputStream.
-     *
-     * The data is read from the ByteArrayInputStream and the size of the data is read first.
-     * Then, for each element in the data, the HashType and the ByteArray are read and added to the result list.
-     *
-     * @param data ByteArray that is going to be deserialized.
+     * Decodes the ByteArray to an object.
+     * The ByteArray is the serialized object that is going to be received using ObjectInputStream.
      */
     fun decode(data: ByteArray): T
 
     /**
      * Executes the message.
-     *
      * This method is used to execute the message and perform the action that is going to be done with the data.
      */
     suspend fun execute(socket: Socket)
