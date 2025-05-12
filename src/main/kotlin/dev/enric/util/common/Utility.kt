dcad524ab441b8cc05aa21866d477822d1c67f19
enric.velasco <enric.velasco@csa.es>
2025-05-12 11:57:02 +0200

    Feat: v1.2.6-BETA

--- src/main/kotlin/dev/enric/util/common/Utility.kt
+++ src/main/kotlin/dev/enric/util/common/Utility.kt
@@ -1,5 +1,10 @@
 package dev.enric.util.common
 
+import dev.enric.core.handler.admin.RemotePathConfig
+import dev.enric.domain.Hash
+import dev.enric.domain.objects.Commit
+import dev.enric.domain.objects.remote.DataProtocol
+import dev.enric.exceptions.RemoteDirectionNotFoundException
 import dev.enric.logger.Logger
 import java.sql.Timestamp
 import java.time.LocalDateTime
@@ -17,6 +22,20 @@ object Utility {
 
     fun formatDateTime(dateTime: Timestamp, format: String): String {
         return dateTime.toLocalDateTime().format(DateTimeFormatter.ofPattern(format))
+    }
+
+    fun getCommitListFrom(startHash: String): List<String> {
+        val commitList = mutableListOf<Hash>()
+        var currentHash: Hash? = Hash(startHash)
+
+        while (currentHash != null) {
+            commitList.add(currentHash)
+
+            val commit = Commit.newInstance(currentHash)
+            currentHash = commit.previousCommit
+        }
+
+        return commitList.map { it.string }
     }
 
     fun printWaitingBar() {
