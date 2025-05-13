63ea8fc534e374a3a93ef47d1dfbdfc6d54c9c93
enric.velasco <enric.velasco@csa.es>
2025-05-13 11:53:55 +0200

    Refactor: Enhance blame functionality with colorized output and streamline line comparison logic

--- src/main/kotlin/dev/enric/util/common/ColorUtil.kt
+++ src/main/kotlin/dev/enric/util/common/ColorUtil.kt
@@ -1,25 +1,39 @@
 package dev.enric.util.common
 
 import picocli.CommandLine.Help.Ansi
+import kotlin.math.absoluteValue
 
 /**
  * Utility class to colorize text in the console
  */
 object ColorUtil {
+    private val colorPool = listOf(
+        "bold,blue", "bold,green", "bold,magenta", "bold,cyan",
+        "bold,yellow", "bold,white", "bold,red"
+    )
 
-    fun title(title : String): String {
+    private val hashColorMap = mutableMapOf<String, String>()
+
+    fun hashColor(hash: String): String {
+        return hashColorMap.getOrPut(hash) {
+            val index = (hash.hashCode().absoluteValue) % colorPool.size
+            "@|${colorPool[index]} $hash|@"
+        }.let { Ansi.ON.string(it) }
+    }
+
+    fun title(title: String): String {
         return Ansi.ON.string("@|cyan ${title}|@")
     }
 
-    fun label(label : String): String {
+    fun label(label: String): String {
         return Ansi.ON.string("@|bold,cyan ${label}|@")
     }
 
-    fun text(text : String): String {
+    fun text(text: String): String {
         return Ansi.ON.string("@|white ${text}|@")
     }
 
-    fun message(message : String): String {
+    fun message(message: String): String {
         return Ansi.ON.string("@|italic,white ${message}|@")
     }
 
